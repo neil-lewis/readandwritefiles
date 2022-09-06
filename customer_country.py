4cf0316139ca63b663fcc5ffc7f4b6f4c4cdc598
Neil <nl@test.com>
2022-09-06 12:59:42 -0500

    customer country headers

--- customer_country.py
+++ customer_country.py
@@ -8,6 +8,8 @@ outfile = open('customer_country.csv', 'w')
 
 next(csvfile)
 
+outfile.write('Full Name, Country\n')
+
 for row in csvfile:
     outfile.write(row[1] + ' ' + row[2] + ', ' + row[4] +'\n')
 
