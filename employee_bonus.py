a9f656a42f0c88874b4c4e51f50e59ae38b9afb1
Neil <nl@test.com>
2022-09-06 22:38:30 -0500

    format changes - bonus

--- employee_bonus.py
+++ employee_bonus.py
@@ -9,7 +9,7 @@ next(csvfile)
 for row in csvfile:
     print('ID:', row[0])
     print('Name:', row[1], row[2])
-    print('Salary:', row[3])
+    print('Salary: ', '$', format(float(row[3]),',.0f'), sep = '')
     print('Bonus: ', float(row[4]) * 100, '%', sep = '')
     salary = (float(row[3]) * float(row[4])) + float(row[3])
     print('Total Pay: ', '$' ,format(salary,',.2f'), sep = '')
